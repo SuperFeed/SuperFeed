920ae6ec5a95b404c09d13810255be14eba14feb
Ryan Delaney <rrdelaney@outlook.com>
2016-03-24 16:01:41 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { SF_API } from './api'
 import re, { selector } from './actions'
 
 @connect(selector, re.action)
 export default class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      version: null,
+      posts: null
+    }
+  }
+
+  async componentDidMount () {
+    let [version, posts] = await Promise.all([
+      SF_API.get('version'),
+      SF_API.get('getPosts')
+    ])
+
+    this.setState({ version, posts })
+  }
+
   render () {
-    return <div className='ui button'>
-      Auth: {JSON.stringify(this.props.auth)}
+    return <div className='ui container'>
+      <p>Auth: {JSON.stringify(this.props.auth)}</p>
+      <p>Version: {JSON.stringify(this.state.version)}</p>
+      <p>Posts: {JSON.stringify(this.state.posts)}</p>
     </div>
   }
 }
--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const req = (host) => ({
-  get: (path) => fetch(`${host}/${path}`).then((res) => res.json()),
+  get: (path) => fetch(`${host}${path}`).then((res) => res.json()),
   post: (path, body) => fetch(host, {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
@@ -8,6 +8,6 @@ const req = (host) => ({
 })
 
 export const SF_API = req(process.env.NODE_ENV !== 'production'
-  ? '//localhost:3005'
-  : '//superfeedapi.azurewebsites.net'
+  ? '//localhost:3005/superfeed_'
+  : 'https://reoz4sq35d.execute-api.us-east-1.amazonaws.com/prod/superfeed_'
 )
