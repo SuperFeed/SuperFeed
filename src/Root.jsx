f42040cb966af301bb0b4d94a6d9fc17e473baeb
Ryan Delaney <rrdelaney@outlook.com>
2016-03-28 14:49:41 -0400

    Fixed lint

--- src/Root.jsx
+++ src/Root.jsx
@@ -9,7 +9,7 @@ export default class Root extends Component {
         title='SuperFeed'
         titleTemplate='SuperFeed - %s'
         meta={[
-          { name:'viewport', content: 'width=device-width, initial-scale=1' }
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' }
         ]}
         link={[
           { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' },
