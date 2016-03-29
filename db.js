515b7560acf3b864373f9546b7f32ade62f597a1
Ryan Delaney <rrdelaney@outlook.com>
2016-03-29 03:25:36 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 module.exports = function connect (db) {
   return process.env.NODE_ENV === 'production'
-    ? { name: 'http://db.superfeed.xyz:5984/' + 'meta', auth: { username: process.env.COUCH_USERNAME, password: process.env.COUCH_PASSWORD } }
+    ? { name: 'https://db.superfeed.xyz/' + db, auth: { username: process.env.COUCH_USERNAME, password: process.env.COUCH_PASSWORD } }
     : { name: 'SFDB' + (db || ''), db: require('memdown') }
 }
