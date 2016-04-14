83eb7f3aa01a5939c55dbcbca021fc88fa8ff30b
Ryan Delaney <rrdelaney@outlook.com>
2016-04-14 18:14:47 -0400

    Updated rethinkdb

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 module.exports.DB = (function connect () {
   return process.env.NODE_ENV === 'production' || process.env.DB_ENV === 'production'
-    ? { host: 'rdb.superfeed.xyz', db: 'sf', authKey: process.env.RDB_AUTH_KEY }
+    ? { host: 'rdb.superfeed.xyz', db: 'sf', user: 'lambda', password: process.env.RDB_AUTH_KEY }
     : { db: 'sf' }
 })()
 
