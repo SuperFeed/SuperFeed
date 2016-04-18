4ebcab492d7f0bc06a620a8cc1aeccb24fab9569
Ryan Delaney <rrdelaney@outlook.com>
2016-04-18 14:20:01 -0400

    Use better soular API

--- src/server.jsx
+++ src/server.jsx
@@ -5,11 +5,12 @@ import soular from 'soular'
 import serveStatic from 'soular/static'
 import ping from 'soular/ping'
 import cors from 'soular/cors'
+import router from 'soular/react-router'
 
 import React from 'react'
 import { renderToString, renderToStaticMarkup } from 'react-dom/server'
 
-import { createMemoryHistory, match, RouterContext } from 'react-router'
+import { createMemoryHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import routes from './routes'
@@ -33,7 +34,7 @@ soular('*')
   }
 })
 
-.use((ctx) => new Promise((resolve, reject) => {
+.use((ctx) => {
   const store = configureStore()
   const initialState = JSON.stringify(store.getState())
   const memoryHistory = createMemoryHistory(ctx.req.url)
@@ -45,44 +46,31 @@ soular('*')
       : '//static.superfeed.xyz/' + require('./stats').main
     : 'app.js'
 
-  match({ history, routes, location: ctx.req.url }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      reject(error)
-    } else if (redirectLocation) {
-      resolve({
-        status: 302,
-        headers: {
-          Location: redirectLocation.pathname + redirectLocation.search
-        }
-      })
-    } else if (renderProps) {
-      const app = renderToString(
-        <Container store={store}>
-          <RouterContext {...renderProps} />
-        </Container>
-      )
+  return router({ history, routes }, (content) => {
+    const app = renderToString(
+      <Container store={store}>
+        {content}
+      </Container>
+    )
 
-      resolve('<!doctype html>' + renderToStaticMarkup(
-        <html lang='en'>
-          <head>
-            <title>SuperFeed</title>
-            <meta name='viewport' content='width=device-width, initial-scale=1' />
-            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' />
-            <link rel='stylesheet' href='https://oss.maxcdn.com/semantic-ui/2.1.8/semantic.min.css' />
-            <script dangerouslySetInnerHTML={{ __html: 'window.__REDUX_INIT = ' + initialState }}></script>
-            <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script>
-          </head>
-          <body>
-            <div id='root' dangerouslySetInnerHTML={{ __html: app }}></div>
-            <script src={appScript}></script>
-          </body>
-        </html>
-      ))
-    } else {
-      resolve()
-    }
-  })
-}))
+    return '<!doctype html>' + renderToStaticMarkup(
+      <html lang='en'>
+        <head>
+          <title>SuperFeed</title>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' />
+          <link rel='stylesheet' href='https://oss.maxcdn.com/semantic-ui/2.1.8/semantic.min.css' />
+          <script dangerouslySetInnerHTML={{ __html: 'window.__REDUX_INIT = ' + initialState }}></script>
+          <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script>
+        </head>
+        <body>
+          <div id='root' dangerouslySetInnerHTML={{ __html: app }}></div>
+          <script src={appScript}></script>
+        </body>
+      </html>
+    )
+  })(ctx)
+})
 
 .use(serveStatic('', DEBUG ? 'resources/static' : 'static'))
 
