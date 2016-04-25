2251b5f4b0b3633abc7a35edbee90bc6a517468c
Ryan Delaney <rrdelaney@outlook.com>
2016-04-25 19:27:53 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ const config = {
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
         STATIC: JSON.stringify(process.env.STATIC || process.env.NODE_ENV || 'local'),
-        FB_ENV: JSON.stringify(process.env.FB_ENV || proess.env.NODE_ENV || 'local')
+        FB_ENV: JSON.stringify(process.env.FB_ENV || process.env.NODE_ENV || 'local')
       }
     })
   ],
